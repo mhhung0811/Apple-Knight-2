1945c6e9783163aef748319c1651a0a0f89a2b6a
mhhung0811 <126455648+mhhung0811@users.noreply.github.com>
2023-12-21 17:44:13 +0700

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed;
+    public float yOffset;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.transform.position.x, target.transform.position.y + yOffset, -10);
+        this.transform.position = Vector3.Slerp(transform.position, newPos, followSpeed*Time.deltaTime);
+    }
+}

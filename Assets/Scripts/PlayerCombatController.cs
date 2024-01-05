56aa61ce96fee96ec6eabd2d483c8a421877d33f
HungMaiHoang <mhhung0811@gmail.com>
2024-01-05 19:08:11 +0700

    add attack animation

--- Assets/Scripts/PlayerCombatController.cs
+++ Assets/Scripts/PlayerCombatController.cs
@@ -7,13 +7,15 @@ public class PlayerCombatController : MonoBehaviour
     [SerializeField]
     private bool combatEnable;
     [SerializeField]
-    private float inputTimer, attackRadius, attackDamage;
+    private float inputTimer, combatTime, attackRadius, attackDamage;
     [SerializeField]
     private Transform attackHitBoxPos;
     [SerializeField]
     private LayerMask WhatIsDamageable;
 
-    private bool gotInput, isAttacking;
+    private bool gotInput, isCombat, isAttacking1, isAttacking2;
+
+    private int countAttack;
 
     private float lastInputTime;
 
@@ -22,6 +24,9 @@ public class PlayerCombatController : MonoBehaviour
     private void Start()
     {
         lastInputTime = Mathf.NegativeInfinity;
+        countAttack = 0;
+        isCombat = false;
+
         anim = GetComponent<Animator>();
         anim.SetBool("canAttack", combatEnable);
     }
@@ -38,22 +43,55 @@ public class PlayerCombatController : MonoBehaviour
             {
                 // Atempt combat
                 gotInput = true;
-                lastInputTime = Time.time;
+                lastInputTime = Time.time;   
             }
-
+        }
+        // Last input time over combat time -> reset combat
+        if (Time.time > lastInputTime + combatTime)
+        {
+            countAttack = 0;
+            anim.SetInteger("countAttack", countAttack);
+            isCombat = false;
+            anim.SetBool("isCombat", isCombat);
         }
     }
     private void CheckAttack()
     {
         if (gotInput)
         {
-            if (!isAttacking)
+            // Start attack
+            if (!isCombat)
             {
-                Debug.Log("attack");
+                // Apply input
                 gotInput = false;
-                isAttacking = true;
-                anim.SetBool("isAttacking", isAttacking);
-            }    
+
+                // Start combat
+                isCombat = true;
+                anim.SetBool("isCombat", isCombat);
+
+                // Perform attack 1
+                isAttacking1 = true;
+                anim.SetBool("isAttacking1", isAttacking1);
+            }
+            // is combating
+            else
+            {
+                // Apply input
+                gotInput = false;
+                if (countAttack == 0 && !isAttacking1)
+                {
+                    // Perform attack 1
+                    isAttacking1 = true;
+                    anim.SetBool("isAttacking1", isAttacking1);
+                }
+
+                if (countAttack == 1 && !isAttacking2)
+                {
+                    // Perform attack 2
+                    isAttacking2 = true;
+                    anim.SetBool("isAttacking2", isAttacking2);
+                }
+            }
         }
         if (Time.time > lastInputTime + inputTimer)
         {
@@ -71,10 +109,21 @@ public class PlayerCombatController : MonoBehaviour
             // Instantiate hit particle
         }
     }
-    private void FinishAttack()
+    private void FinishAttack1()
     {
-        isAttacking = false;
-        anim.SetBool("isAttacking", false);
+        isAttacking1 = false;
+        anim.SetBool("isAttacking1", isAttacking1);
+        countAttack = 1;
+        anim.SetInteger("countAttack", countAttack);
+    }
+    private void FinishAttack2()
+    {
+        isAttacking2 = false;
+        anim.SetBool("isAttacking2", isAttacking2);
+        // Last attack
+        countAttack = 0;
+        anim.SetInteger("countAttack", countAttack);
+
     }
     //private void OnDrawGizmos()
     //{

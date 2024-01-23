2738a77e8dc6c35ae8a6f2569f28017570fe3a11
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-01-23 16:29:01 +0700

    Them TakeDamge()

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : ScriptableObject
+{
+    public float maxHP = 100;
+    public float HP = 100;
+    public float damage = 10;
+    public float speed = 10;
+}

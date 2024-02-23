d649beab2001fe1fe36be5dce1a63cdc597dc4ba
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-02-23 22:24:43 +0700

    Cannon

---
+++ Assets/Scripts/Bullet/Cannon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField]
+    private float delayTime;
+    [SerializeField]
+    private float coolDownFire;
+    void Start()
+    {
+        StartFire();
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void Fire()
+    {
+        Debug.Log("hêlo");
+
+        GameObject fireBall = BulletManager.Instance.TakeFireBall();
+        FireBall fb = fireBall.GetComponent<FireBall>();
+        fb.transform.position = this.transform.position;
+        fb.transform.rotation = this.transform.rotation;
+        fb.SetUpSpecific(this.transform.rotation.eulerAngles.z);
+    }
+
+    private void StartFire()
+    {
+        InvokeRepeating("Fire", delayTime, coolDownFire);
+    }
+}

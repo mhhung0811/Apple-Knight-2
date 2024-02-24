c1d828fe51acb3cbf1ce32e34a7a8367cf36531f
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-02-24 10:33:43 +0700

    Test2

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -55,6 +55,7 @@ public class GameManager : MonoBehaviour
     public void LoadGame()
     {
         LoadData?.Invoke();
+    }
     public void ButtonQuitGame()
     {
         SceneManager.LoadScene("Start Game");
